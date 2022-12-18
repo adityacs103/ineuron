7b2fb3b2796e3747a5882fb894afd5d703a5ec95
Aditya kumar <adityacs103@gmail.com>
2022-12-18 12:14:32 +0530

    basic flask is complited, task for flask connect with mongodb

--- python_project/test.py
+++ python_project/test.py
@@ -2,14 +2,43 @@ from flask import Flask,request,jsonify
 
 app=Flask(__name__)
 
-@app.route('/abc',methods=['GET','POST'])
+@app.route('/add',methods=['GET','POST'])
 
-def test1():
+def add():
     if(request.method=='POST'):
         a=request.json['num1']
         b=request.json['num2']
         result=a+b
         return jsonify(str(result))
+
+
+@app.route("/sub",methods=['GET','POST'])
+def sub():
+    if(request.method=='POST'):
+        a=request.json['num1']
+        b=request.json['num2']
+        result=a-b
+        return jsonify((str(result)))
+
+
+@app.route("/multiply",methods=['GET','POST'])
+def multiply():
+    if(request.method=='POST'):
+        a=request.json['num1']
+        b=request.json['num2']
+        result=a*b
+        return jsonify((str(result)))
+
+
+@app.route("/devide",methods=['GET','POST'])
+def devide():
+    if(request.method=='POST'):
+        a=request.json['num1']
+        b=request.json['num2']
+        result=a/b
+        return jsonify(result)
+
+
 if __name__=='__main__':
     app.run()
 
