507e772140504ee6855ccb04b0cc9038ab31cdcc
Aditya Kumar <adityacs103@gmail.com>
2023-07-06 13:30:30 +0530

    arranging some files in folder

--- practice.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector as connection
-from sqlalchemy import VARCHAR
-mydb=connection.connect(host="localhost",user="root",passwd="11223344")
-print(mydb)
-cursor=mydb.cursor()
-cursor.execute("show databases")
-print(cursor.fetchall())
--- test3.py
+++
@@ -1,8 +0,0 @@
-import logging
-logging.basicConfig(filename="Test3.log",level=logging.DEBUG,format='%(levelname)s %(asctime)s %(name)s %(message)s')
-try:
-    with open('sudh.txt',"r"):
-        logging.info("Succesfully it has read the file")
-except Exception as e:
-    logging.critical("This a solution for us")
-    logging.error(e)
